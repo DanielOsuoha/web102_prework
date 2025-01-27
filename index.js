f36f4d1f30396d614ca1a467b09e0ec1dbaf1f7b
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-01-27 00:20:04 -0600

    added the games

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function addGamesToPage(games) {
         // set the inner HTML using a template literal to display some info 
         // about each game
         gameCard.innerHTML = `
-            <img src="${game.img}" alt="${game.name}" />
+            <img src="${game.img}" alt="${game.name}" class="game-img" />
             <h3>${game.name}</h3>
             <p>${game.description}</p>
             <p>Pledged: $${game.pledged.toLocaleString()}</p>
